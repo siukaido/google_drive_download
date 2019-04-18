0727537eb27f372dc3f7421b459050af0d89b207
siukaido <siukaido@gmail.com>
2019-04-18 18:09:53 +0900

    recursiveに対応

--- run.rb
+++ run.rb
@@ -2,32 +2,28 @@ require "bundler/setup"
 require "google_drive"
 require "color_echo"
 require "parallel"
+require "optparse";
 
-CE.pickup("[Failed]", :red).pickup("[Successful]", :h_blue)
-
-session = GoogleDrive::Session.from_config("config.json")
-
-unless ARGV[0]
-  puts "[Failed] URLをいれてください"
-  exit 1
+def usage
+  puts "Usage: ruby run.rb [OPTION]... <SourceURL> <DistPath>"
+  puts
+  puts "Mandatory arguments to long options are mandatory for short options too."
+  puts "  --name  SourceURL直下で対応するファイル/ディレクトリ名"
+  puts "  -h      ヘルプ表示"
 end
 
-unless ARGV[1]
-  puts "[Failed] 保存先のディレクトリを指定してください"
-  exit 1
-end
-
-begin
-  collection = session.collection_by_url(ARGV[0])
-rescue => e
-  puts "[Failed] 指定URLのドライブにアクセスできません"
-  exit 1
-end
-
-Parallel.each(collection.files, in_threads: 5) do |file|
-  filename = File.join(ARGV[1], file.title)
+def download(basepath, file)
+  filename = File.join(basepath, file.title)
   begin
-    if file.is_a?(GoogleDrive::Spreadsheet)
+    if file.is_a?(GoogleDrive::Collection)
+      begin
+        Dir.mkdir(filename)
+      rescue
+      end
+      file.files.each do |file2|
+        download(filename, file2)
+      end
+    elsif file.is_a?(GoogleDrive::Spreadsheet)
       filename += ".csv"
       file.export_as_file(filename, "text/csv")
     else
@@ -38,4 +34,40 @@ Parallel.each(collection.files, in_threads: 5) do |file|
     puts "[Failed] " + file.title + " のダウンロードに失敗しました"
     p e
   end
+  return filename
+end
+
+CE.pickup("[Failed]", :red).pickup("[Successful]", :h_blue)
+params = ARGV.getopts("h", "name:")
+
+if params["h"]
+  usage
+  exit
+end
+
+unless ARGV[0]
+  puts "[Failed] URLをいれてください"
+  usage
+  exit 1
+end
+unless ARGV[1]
+  puts "[Failed] 保存先のディレクトリを指定してください"
+  usage
+  exit 1
+end
+
+session = GoogleDrive::Session.from_config("config.json")
+begin
+  collection = session.collection_by_url(ARGV[0])
+rescue => e
+  puts "[Failed] 指定URLのドライブにアクセスできません"
+  exit 1
+end
+
+Parallel.each(collection.files, in_threads: 5) do |file|
+  if params["name"]
+    next unless file.title.match(params["name"])
+  end
+
+  download(ARGV[1], file)
 end
